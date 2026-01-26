708f6612fc8a7ddd9561cd4708e635f70a6be087
Sylvain Gourvil <sg@quable.fr>
2026-01-26 16:05:05 +0100

    SECO-419: Npm dev banner (chore)

---
+++ scripts/banner.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+console.log('\n');
+console.log('\x1b[36m\x1b[1m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\x1b[0m');
+console.log('\x1b[35m\x1b[1m🚀 QuableApp - Development Server\x1b[0m');
+console.log('\x1b[36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\x1b[0m');
+console.log('\x1b[32m📖 Documentation:\x1b[0m http://localhost:3000/readme');
+console.log('\x1b[32m💡 Examples:\x1b[0m     See test.http file');
+console.log('\x1b[32m🎨 Storybook:\x1b[0m      https://quable-ui-storybook.web.app/');
+console.log('\x1b[32m🎨 Quable UI npm package:\x1b[0m      https://www.npmjs.com/package/@quable/ui?activeTab=readme');
+console.log('\x1b[32m🎨 Quable JS npm package:\x1b[0m      https://www.npmjs.com/package/@quable/quable-pim-js?activeTab=readme');
+console.log('\x1b[32m🎨 Quable CLI npm package:\x1b[0m      https://www.npmjs.com/package/@quable/quable-cli?activeTab=readme');
+console.log('\x1b[36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\x1b[0m');
+console.log('\x1b[33m💡 Press Ctrl+C to stop the server\x1b[0m');
+console.log('\x1b[36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\x1b[0m\n');
